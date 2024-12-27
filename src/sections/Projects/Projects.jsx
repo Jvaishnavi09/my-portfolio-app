0b6f62013df8ec98e53e318d1de6484ac3a1e95f
Jvaishnavi09 <jayavaishnavi092000@gmail.com>
2024-12-27 21:59:37 +0530

    New Project Youtube Clone Added

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import styles from "./ProjectsStyles.module.css";
 import hoobank from "../../assets/Desktop - 1.png";
 import ProjectCard from "../../common/ProjectCard";
-
+import youtubeClone from "../../assets/YOUTUBE CLONE.png";
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
@@ -12,6 +12,12 @@ function Projects() {
           link="https://spiffy-pasca-9e0459.netlify.app/"
           h3="Web Frontend"
           p="Banking App- React and Tailwind"
+        />
+        <ProjectCard
+          src={youtubeClone}
+          link="https://bright-cat-2b9d4b.netlify.app/"
+          h3="Web Frontend-API"
+          p="Youtube Clone -React and Material UI"
         />
       </div>
     </section>
