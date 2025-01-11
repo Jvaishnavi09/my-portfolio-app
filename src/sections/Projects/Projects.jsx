cebf4dc3948abda44b5c7109af311dfa3a1b7a34
Jvaishnavi09 <jayavaishnavi092000@gmail.com>
2025-01-11 23:35:25 +0530

    new project recruitly added

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import styles from "./ProjectsStyles.module.css";
 import hoobank from "../../assets/Desktop - 1.png";
 import ProjectCard from "../../common/ProjectCard";
 import youtubeClone from "../../assets/YoutubeClonePic.png";
+import recruitly from "../../assets/recruitly.png";
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
@@ -20,6 +21,14 @@ function Projects() {
           link="https://bright-cat-2b9d4b.netlify.app/"
           h3="Web Frontend-API"
           p="Youtube Clone -React and Material UI"
+          height="250px"
+          width="150px"
+        />
+        <ProjectCard
+          src={recruitly}
+          link="https://recruitly.onrender.com/"
+          h3="Backened MVC-Ejs"
+          p="Job Portal"
           height="250px"
           width="150px"
         />
